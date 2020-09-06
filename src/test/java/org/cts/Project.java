8726458a92aff64edd5f09caf98c68beb64f99b0
srikanth1327 <srikanthrgvm@gmail.com>
2020-09-06 15:29:04 +0530

    done

--- src/test/java/org/cts/Project.java
+++ src/test/java/org/cts/Project.java
@@ -1,5 +1,16 @@
 package org.cts;
 
 public class Project {
+	
+	public static void main(String[] args) {
+		
+		String s = "HELLO SRIK";
+		System.out.println(s);
+		System.out.println("successfully done");
+		
+		int a=10;
+		int b=20;
+		
+	}
 
 }
