57cab1e6760ba972c54ef7b46832742a133c5872
srikanth1327 <srikanthrgvm@gmail.com>
2020-09-06 15:42:10 +0530

    done

--- src/test/java/org/cts/Project.java
+++ src/test/java/org/cts/Project.java
@@ -2,20 +2,16 @@ package org.cts;
 
 public class Project {
 	
-<<<<<<< HEAD
+
 	public static void main(String[] args) {
-		
+
+	    int a=10;
+	    int b=20;
 		String s = "HELLO SRIK";
 		System.out.println(s);
 		System.out.println("successfully done");
 		
-		int a=10;
-		int b=20;
 		
 	}
-=======
-	int a=10;
-	int b=20;
->>>>>>> 9fd8f7674b469426385cb2365c23fb48eea5ad30
 
 }
